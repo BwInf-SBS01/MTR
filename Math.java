14c96f377db19222c9f7516a5714ba7d035381ef
SBSJavatar <31701319+SBSJavatar@users.noreply.github.com>
2017-09-16 18:54:57 +0200

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,13 +1,30 @@
-/**
-  *
-  * Beschreibung
-  *
-  * @version 1.0 vom 12.09.2017
-  * @author 
-  */
 
-public class Math {
+  public class Math {
+    
+ +  public static double Addition(double a, double b){
+ +    return(a+b);
+ +  }
+    
+ +  public double Substraktion(double a, double b){
+ +    return(a-b);
+ +  }
+ +  
+ +  public double  Multiplikation(double a, double b){
+ +    return(a*b);
+ +  }
+ +  
+ +  public double Division(double a, double b){
+ +    return(a/b);
+ +  } 
+ +  
+ +  public double Fakultät(int a){
+ +    for (a;a <0 ;a-- ) {
+ +      
+ +    } // end of for
+ +  }  
+ +  public double Integration(){
+ +      
+ +  }
   
-  
-
-} // end of class Math
+ +  
+  } // end of class Math
