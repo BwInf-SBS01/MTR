265c787ed9912dfd91ef42147dfe728cd2683dca
SBSJavatar <31701319+SBSJavatar@users.noreply.github.com>
2017-09-19 08:49:19 +0200

    Update Parser.java
    ehjioorhioe4r

--- Parser.java
+++ Parser.java
@@ -3,79 +3,124 @@
   * Beschreibung
   *
   * @version 1.0 vom 12.09.2017
-  * @author 
+  * @author Niklas
   */
 
-package de.taschenrechner.java;
-
 public class Parser {
-
-	public Parser() {
-
-	}
-
-	private static String nextOperation(String input) {
-		String operation = "";
-		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) == '*') {
-				operation = "mal";
-				break;
-			} else if (input.charAt(i) == '/') {
-				operation = "geteilt";
-				break;
-			}
-		}
-		return operation;
-	}
-
-	private static int nextOperationIndex(String input) {
-		int index = input.length();
-		if (nextOperation(input).equals("mal")) {
-			index = input.indexOf('*');
-		} else if (nextOperation(input).equals("geteilt")) {
-			index = input.indexOf('/');
-		}
-
-		return index;
-	}
-
-	private String punktRechnen(String rechnung) {
-		
-		if (nextOperation(rechnung).equals("mal")) {
-			int i1 = rechnung.indexOf('*');
-		
-			int i2 = nextOperationIndex(rechnung.substring(i1 + 1)) + i1 + 1;
-
-	
-			double doubleErgebnis = Double.parseDouble(rechnung.substring(0, i1)) * Double.parseDouble(rechnung.substring(i1 + 1, i2));
-		
-			rechnung = Double.toString(doubleErgebnis) + rechnung.substring(i2, rechnung.length());
-			System.out.println(rechnung);
-			rechnung = punktRechnen(rechnung);
-			
-		} else if (nextOperation(rechnung).equals("geteilt")) {
-			int i1 = rechnung.indexOf('/');
-	
-			int i2 = nextOperationIndex(rechnung.substring(i1 + 1)) + i1 + 1;
-	
-			double doubleErgebnis = Double.parseDouble(rechnung.substring(0, i1))
-					/ Double.parseDouble(rechnung.substring(i1 + 1, i2));
-		
-			rechnung = Double.toString(doubleErgebnis) + rechnung.substring(i2, rechnung.length());
-			System.out.println(rechnung);
-			rechnung = punktRechnen(rechnung);
-		} 	
-		return rechnung;
-	}
-
-	public static void main(String[] args) {
-		String str = "1+2*(3+4)";
-		Parser p = new Parser();
-		// System.out.println(p.verarbeiten(p.vorbereiten(str), 0,
-		// str.length()+2));
-		System.out.println(p.punktRechnen("100*10/100*2"));      //obwohl es davor richtig ausgegeben wurde wird
-                                                           //der um die erste Rechnung gekürzte String ausgegeben
-
-	}
-
-} // end of class Parser
+  
+  public Parser() {
+    
+  }
+  
+  private static String strichRechnen(String rechnung) {
+    
+    if (nextOperationStrich(rechnung).equals("add")) {
+      int i1 = rechnung.indexOf('+');
+      int i2 = nextOperationIndexStrich(rechnung.substring(i1 + 1)) + i1 + 1;
+      
+      double doubleErgebnis = Double.parseDouble(punktRechnen(rechnung.substring(0, i1))) + Double.parseDouble(punktRechnen(rechnung.substring(i1+1, i2)));
+      System.out.println("Rechnung: " + rechnung.substring(0, i1) + " + " + rechnung.substring(i1 + 1, i2));
+      rechnung = Double.toString(doubleErgebnis) + rechnung.substring(i2);
+      System.out.println(rechnung);
+      rechnung = strichRechnen(rechnung);
+    } else if(nextOperationStrich(rechnung).equals("sub")) {
+      int i1 = rechnung.indexOf('-');
+      int i2 = nextOperationIndexStrich(rechnung.substring(i1 + 1)) + i1 + 1;
+      
+      double doubleErgebnis = Double.parseDouble(punktRechnen(rechnung.substring(0, i1))) - Double.parseDouble(punktRechnen(rechnung.substring(i1+1, i2)));
+      System.out.println("Rechnung: " + rechnung.substring(0, i1) + " - " + rechnung.substring(i1 + 1, i2));
+      rechnung = Double.toString(doubleErgebnis) + rechnung.substring(i2);
+      System.out.println(rechnung);
+      rechnung = strichRechnen(rechnung);
+    }
+    return rechnung;
+  }  
+  
+  
+  
+  private static String punktRechnen(String rechnung) {
+    
+    if (nextOperationPunkt(rechnung).equals("mal")) {
+      int i1 = rechnung.indexOf('*');
+      int i2 = nextOperationIndexPunkt(rechnung.substring(i1 + 1)) + i1 + 1;
+      
+      
+      double doubleErgebnis = Double.parseDouble(rechnung.substring(0, i1)) * Double.parseDouble(rechnung.substring(i1 + 1, i2));
+      System.out.println("Rechnung: " + rechnung.substring(0, i1) + " * " + rechnung.substring(i1 + 1, i2));
+      rechnung = Double.toString(doubleErgebnis) + rechnung.substring(i2, rechnung.length());
+      System.out.println(rechnung);
+      rechnung = punktRechnen(rechnung);
+      
+    } else if (nextOperationPunkt(rechnung).equals("geteilt")) {
+      int i1 = rechnung.indexOf('/');
+      
+      int i2 = nextOperationIndexPunkt(rechnung.substring(i1 + 1)) + i1 + 1;
+      
+      double doubleErgebnis = Double.parseDouble(rechnung.substring(0, i1))
+      / Double.parseDouble(rechnung.substring(i1 + 1, i2));
+      System.out.println("Rechnung: " + rechnung.substring(0, i1) + " / " + rechnung.substring(i1 + 1, i2));
+      rechnung = Double.toString(doubleErgebnis) + rechnung.substring(i2, rechnung.length());
+      System.out.println(rechnung);
+      rechnung = punktRechnen(rechnung);
+    }   
+    return rechnung;
+  }
+  
+  private static String nextOperationStrich(String input) {
+    String operation = "";
+    for (int i = 0; i < input.length(); i++) {
+      if (input.charAt(i) == '+') {
+        operation = "add";
+        break;
+      } else if (input.charAt(i) == '-') {
+        operation = "sub";
+        break;
+      }
+    }
+    return operation;
+  }
+  
+  private static int nextOperationIndexStrich(String input) {
+    int index = input.length();
+    if (nextOperationStrich(input).equals("add")) {
+      index = input.indexOf('+');
+    } else if (nextOperationStrich(input).equals("sub")) {
+      index = input.indexOf('-');
+    }
+    
+    return index;
+  }  
+  private static String nextOperationPunkt(String input) {
+    String operation = "";
+    for (int i = 0; i < input.length(); i++) {
+      if (input.charAt(i) == '*') {
+        operation = "mal";
+        break;
+      } else if (input.charAt(i) == '/') {
+        operation = "geteilt";
+        break;
+      }
+    }
+    return operation;
+  }
+  
+  private static int nextOperationIndexPunkt(String input) {
+    int index = input.length();
+    if (nextOperationPunkt(input).equals("mal")) {
+      index = input.indexOf('*');
+    } else if (nextOperationPunkt(input).equals("geteilt")) {
+      index = input.indexOf('/');
+    }
+    
+    return index;
+  }
+  
+  
+  public static void main(String[] args) {
+    String str = "1+2*(3+4)";
+    Parser p = new Parser();
+    System.out.println(strichRechnen("3+5*6-7/3"));
+    
+  }
+  
+}
