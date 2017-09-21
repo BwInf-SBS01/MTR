736f5c35b5d956a91b5f8982fdf6410700ab46b2
SBSJavatar <31701319+SBSJavatar@users.noreply.github.com>
2017-09-21 15:44:08 +0200

    Add files via upload

---
+++ Abreisszettel.java
@@ -0,0 +1,48 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.WindowConstants;
+
+public class Abreisszettel extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Gbo gbo;
+	
+	private JList<String> lAbreisszettel = new JList<String>();
+	private DefaultListModel<String> lAbreisszettelMODEL = new DefaultListModel<String>();
+	private JScrollPane lAbreisszettelSCROLLPANE = new JScrollPane(lAbreisszettel);
+
+	public Abreisszettel(Gbo gbo) {
+		super();
+		this.gbo = gbo;
+		lAbreisszettelMODEL.addElement("ABRISSZETTEL:");
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		int frameWidth = 200;
+		int frameHeight = 300;
+		setSize(frameWidth, frameHeight);
+		updateLoaction();
+		setResizable(false);
+		this.setUndecorated(true);
+		Container cp = getContentPane();
+		cp.setLayout(null);
+		this.setLayout(new GridLayout());
+		lAbreisszettel.setModel(lAbreisszettelMODEL);
+
+		cp.add(lAbreisszettelSCROLLPANE);
+		setVisible(true);
+	}
+
+	public void addElementToList(String string) {
+		if (!string.isEmpty()) {
+			lAbreisszettelMODEL.addElement(string);
+		}
+	}
+	public void updateLoaction() {
+		setLocation(gbo.getX() + gbo.getWidth(), gbo.getY());
+	}
+
+}
