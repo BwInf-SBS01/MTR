4fee83fe25855da1fa37adfc2365ee2aa559e54b
SBSJavatar <31701319+SBSJavatar@users.noreply.github.com>
2017-09-28 12:29:16 +0200

    Add files via upload

--- Abreisszettel.java
+++ Abreisszettel.java
@@ -10,46 +10,46 @@ import javax.swing.WindowConstants;
 
 public class Abreisszettel extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	private Gbo gbo;
+  private Gbo gbo;
 
-	private JList<String> lAbreisszettel = new JList<String>();
-	private DefaultListModel<String> lAbreisszettelMODEL = new DefaultListModel<String>();
-	private JScrollPane lAbreisszettelSCROLLPANE = new JScrollPane(lAbreisszettel);
+  private JList<String> lAbreisszettel = new JList<String>();
+  private DefaultListModel<String> lAbreisszettelMODEL = new DefaultListModel<String>();
+  private JScrollPane lAbreisszettelSCROLLPANE = new JScrollPane(lAbreisszettel);
 
-	public Abreisszettel(Gbo gbo) {
-		super();
-		this.gbo = gbo;
-		lAbreisszettelMODEL.addElement("ABRISSZETTEL:");
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		int frameWidth = 200;
-		int frameHeight = 300;
-		setSize(frameWidth, frameHeight);
-		updateLoaction();
-		setResizable(false);
-		this.setUndecorated(true);
-		Container cp = getContentPane();
-		cp.setLayout(null);
-		this.setLayout(new GridLayout());
-		lAbreisszettel.setModel(lAbreisszettelMODEL);
+  public Abreisszettel(Gbo gbo) {
+    super();
+    this.gbo = gbo;
+    lAbreisszettelMODEL.addElement("ABRISSZETTEL:");
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    int frameWidth = 200;
+    int frameHeight = 300;
+    setSize(frameWidth, frameHeight);
+    updateLoaction();
+    setResizable(false);
+    this.setUndecorated(true);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    this.setLayout(new GridLayout());
+    lAbreisszettel.setModel(lAbreisszettelMODEL);
 
-		cp.add(lAbreisszettelSCROLLPANE);
-		setVisible(true);
-	}
+    cp.add(lAbreisszettelSCROLLPANE);
+    setVisible(true);
+  }
 
-	public void addElementToList(String string) {
-		if (!string.isEmpty()) {
-			lAbreisszettelMODEL.addElement(string);
-		}
-	}
+  public void addElementToList(String string) {
+    if (!string.isEmpty()) {
+      lAbreisszettelMODEL.addElement(string);
+    }
+  }
 
-	public void updateLoaction() {
-		Point p = new Point(gbo.getX() + gbo.getWidth(), gbo.getY());
-		if (!(getLocation().equals(p))) {
-			setLocation(gbo.getX() + gbo.getWidth(), gbo.getY());
-			gbo.requestFocus();
-		}
-	}
+  public void updateLoaction() {
+    Point p = new Point(gbo.getX() + gbo.getWidth()+10, gbo.getY());
+    if (!(getLocation().equals(p))) {
+      setLocation(gbo.getX() + gbo.getWidth()+10, gbo.getY());
+      gbo.requestFocus();
+    }
+  }
 
 }
--- MathMethoden.java
+++ MathMethoden.java
@@ -7,82 +7,82 @@
  */
 
 public class MathMethoden {
-	private Parser p;
-
-	public MathMethoden(Parser p) {
-		this.p = p;
-	}
-
-	public double Addition(double a, double b) {
-		double ergebnis = a + b;
-		return ergebnis;
-	}
-
-	public double Subtraktion(double a, double b) {
-		double ergebnis = a - b;
-		return ergebnis;
-	}
-
-	public double Multiplikation(double a, double b) {
-		double ergebnis = a * b;
-		return ergebnis;
-	}
-
-	public double Division(double a, double b) {
-		double ergebnis = a / b;
-		return ergebnis;
-	}
-
-	public double Potenz(double a, double e) {
-		double ergebnis = Math.pow(a, e);
-		return ergebnis;
-	}
-
-	public double Sinus(double x) {
-		double ergebnis = Math.sin(x);
-		return ergebnis;
-	}
-
-	public double Cosinus(double x) {
-		double ergebnis = Math.cos(x);
-		return ergebnis;
-	}
-
-	public double Tangens(double x) {
-		double ergebnis = Math.tan(x);
-		return ergebnis;
-	}
-
-	public double Fakultät(int a) {
-		int ergebnis = a;
-		for (int i = 1; i < a; i++) {
-			ergebnis *= i;
-		} // end of for
-		return ergebnis;
-	}
-
-	public int Produktfunktion(double o, double u, String f) {
-		f = f.replace("X", "x");
-		int ergebnis = (int) (u * o);
-		for (int i = (int) (u + 1); i < o; i++) {
-			ergebnis *= p.parsen(f.replaceAll("x", Integer.toString(i)));
-		} 
-		return ergebnis;
-	}
-
-	public double Integration(double u, double o, String f) {
-		f = f.replace("X", "x");
-		double ergebnis = 0;
-		double streifen = 1000;
-		double breite = (o - u) / streifen;
-		double stelle = u;
-		while (stelle < o) {
-			ergebnis = ergebnis + breite * p.parsen(f.replaceAll("x", Double.toString(stelle)));
-			System.out.println(ergebnis);
-			stelle = stelle + breite;
-			System.out.println(stelle);
-		} // end of for
-		return ergebnis;
-	}
-
+  private Parser p;
+  
+  public MathMethoden(Parser p) {
+    this.p = p;
+  }
+  
+  public double Addition(double a, double b) {
+    double ergebnis = a + b;
+    return ergebnis;
+  }
+  
+  public double Subtraktion(double a, double b) {
+    double ergebnis = a - b;
+    return ergebnis;
+  }
+  
+  public double Multiplikation(double a, double b) {
+    double ergebnis = a * b;
+    return ergebnis;
+  }
+  
+  public double Division(double a, double b) {
+    double ergebnis = a / b;
+    return ergebnis;
+  }
+  
+  public double Potenz(double a, double e) {
+    double ergebnis = Math.pow(a, e);
+    return ergebnis;
+  }
+  
+  public double Sinus(double x) {
+    double ergebnis = Math.sin(x);
+    return ergebnis;
+  }
+  
+  public double Cosinus(double x) {
+    double ergebnis = Math.cos(x);
+    return ergebnis;
+  }
+  
+  public double Tangens(double x) {
+    double ergebnis = Math.tan(x);
+    return ergebnis;
+  }
+  
+  public double Fakultät(int a) {
+    int ergebnis = a;
+    for (int i = 1; i < a; i++) {
+      ergebnis *= i;
+    } // end of for
+    return ergebnis;
+  }
+  
+  public double Produktfunktion(double u, double o, String f) {
+    f = f.replace("X", "x");
+    double ergebnis = (1);
+    for (int i = (int) (u + 1); i <= o; i++) {
+      ergebnis *= p.parsen(f.replaceAll("x", Integer.toString(i)));
+    } 
+    return ergebnis;
+  }
+  
+  public double Integration(double u, double o, String f) {
+    f = f.replace("X", "x");
+    double ergebnis = 0;
+    double streifen = 1000;
+    double breite = (o - u) / streifen;
+    double stelle = u;
+    while (stelle < o) {
+      ergebnis = ergebnis + breite * p.parsen(f.replaceAll("x", Double.toString(stelle)));
+      System.out.println(ergebnis);
+      stelle = stelle + breite;
+      System.out.println(stelle);
+    } // end of for
+    return ergebnis;
+  }
+  
 }
