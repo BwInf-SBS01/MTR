a138c91f456557713f4b3a06f7578efe2b993627
SBSJavatar <31701319+SBSJavatar@users.noreply.github.com>
2017-10-01 12:06:48 +0200

    Update TRE.java

--- TRE.java
+++ TRE.java
@@ -1,7 +1,8 @@
 public class TRE extends IllegalArgumentException {
 	private static final long serialVersionUID = 1L;
-
+	public String message;
 	public TRE(String message) {
 		super(message);
+		this.message = message;
 	}
 }
